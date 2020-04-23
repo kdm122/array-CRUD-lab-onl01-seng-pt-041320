8f9d1ab5d989d52683b76acd1d971516ecdb48a6
Kyle McLean <kyledmclean@gmail.com>
2020-04-23 01:15:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["pizza","hotdog","hamburger"]
+  array = ["pizza","hotdog","hamburger",'corndog']
 end
 
 def add_element_to_end_of_array(array, element)
